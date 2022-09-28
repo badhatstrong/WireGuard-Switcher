c2edcbae5cab4208307f247e245a4de94e9381ef
badhatstrong <56743493+badhatstrong@users.noreply.github.com>
2022-09-28 16:31:48 +0400

    Source file

---
+++ WireGuard Switcher.ps1
@@ -0,0 +1,13 @@
+﻿$TunnelPath =  (Get-ChildItem 'C:\Program Files\WireGuard\Data\Configurations\*.conf*' | Select-Object -ExpandProperty Name).tostring()
+$TunnelName = $TunnelPath -replace ".conf.dpapi"
+$HostIP = (Get-NetRoute -DestinationPrefix 0.0.0.0/0 | Select-Object -ExpandProperty NextHop).ToString()
+# write $HostIP # – uncomment this if you want to make sure IP address is correct
+
+if($HostIP -like "192.168.*") # IP address of office DHCP-server
+{
+wireguard /uninstalltunnelservice $TunnelName
+}
+else
+{
+wireguard /installtunnelservice "C:\Program Files\WireGuard\Data\Configurations\$TunnelPath"
+}
